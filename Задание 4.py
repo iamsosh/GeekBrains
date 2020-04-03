49a40c124e2a0ee5b93a3371d14fe70249aa49fb
iamsosh <62951047+iamsosh@users.noreply.github.com>
2020-04-03 12:33:13 +0300

    Добавил задания
    Добавил задания и описания к задачам

---
+++ Задание 4.py
@@ -0,0 +1,18 @@
+#Пользователь вводит целое положительное число.
+#Найдите самую большую цифру в числе.
+#Для решения используйте цикл while и арифметические операции.
+chislo = (int(input('Введите целое число ')))
+ostatok = 0
+while a > 1 :
+	if ostatok == 0 :
+		ostatok = chislo % 10
+		maximum = ostatok
+		chislo //= 10
+	else :
+		ostatok = chislo % 10
+		if maximum < ostatok :
+			maximum = ostatok
+			chislo //= 10
+		else :
+			chislo //= 10
+print(maximum)
